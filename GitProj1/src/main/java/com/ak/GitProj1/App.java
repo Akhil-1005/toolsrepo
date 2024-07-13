a77652fcb746e032f618e960c9f06d6cccc3016f
akhil <akhil@Akhil>
2024-07-13 09:59:32 +0530

    Intial commit

---
+++ GitProj1/src/main/java/com/ak/GitProj1/App.java
@@ -0,0 +1,13 @@
+package com.ak.GitProj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
