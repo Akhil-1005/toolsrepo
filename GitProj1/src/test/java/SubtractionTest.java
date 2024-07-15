87201c7cb3cfd63b2edd925a8b9a97d1cb9bc593
akhil <akhil@Akhil>
2024-07-15 09:53:47 +0530

    updated test class file in subtraction

--- GitProj1/src/test/java/SubtractionTest.java
+++ GitProj1/src/test/java/SubtractionTest.java
@@ -1,9 +1,13 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 import com.ak.GitProj1.Subtraction;
 
 public class SubtractionTest {
 	
+	
+	@Test
 	public void testWithPostiveNumber() {
 		Subtraction sub1=new Subtraction();
 		int actual=sub1.sub(20, 10);
